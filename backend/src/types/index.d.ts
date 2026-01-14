73e5bff1745c1c55bfdc431936ff82b1cbea14a2
“Howard <higamelsa430@gmail.com>
2026-01-13 16:55:27 -0800

    fix: Modified the configuration in order to build successfully

--- backend/src/types/index.d.ts
+++ backend/src/types/index.d.ts
@@ -1,5 +1,15 @@
 import { Role } from '@prisma/client';
 
+declare module 'express' {
+  export interface Request {
+    user?: {
+      userId: string;
+      name: string;
+      role: Role;
+    };
+  }
+}
+
 declare global {
   namespace Express {
     interface Request {
